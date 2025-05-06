b64da1f29b08bef511c5a3f88aff779ba54389b8
Anjali Choudhary <anjalichoudhary1202@gmail.com>
2025-05-06 22:03:19 +0530

    installed codemirror package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Vite configuration to resolve Codemirror files
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'codemirror',
+      'codemirror/lib/codemirror.css',
+      'codemirror/theme/material.css',
+      'codemirror/mode/xml/xml',
+      'codemirror/mode/javascript/javascript',
+      'codemirror/mode/css/css',
+      'codemirror/addon/edit/closebrackets',
+      'codemirror/addon/edit/closetag',
+      'codemirror/addon/comment/comment',
+    ],
+  },
 })
